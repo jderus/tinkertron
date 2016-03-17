47a9f2d262ec467421928c6a1a0be08aa51d88bf
jderus <jderus@gmail.com>
2016-03-16 17:59:30 -0700

    sass and add a tile thinger.

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1300, height: 700});
   mainWindow.setMenu(null); // remove the default menu
   
   // and load the index.html of the app.
